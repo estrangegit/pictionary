f677eaf6727ca2bf13cae594940853d224da4192
estrangegit <etienne.estrangin@gmail.com>
2020-04-27 15:15:09 +0200

    déplacement de l'ensemble du code au niveau d'un dossier src

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'public/ejs/'));
 
 app.use(cookieParser());
 
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/pictionary', routes);
 
